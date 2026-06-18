9d8873903835034fdb46c55465d7a9b13af94cdf
whynky <whynky@gmail.com>
2026-06-18 14:07:29 -0600

    completed assignment 6.5

--- alert_button.py
+++ alert_button.py
@@ -1,13 +1,4 @@
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-button_pressed = False
-while True:
-    if GPIO.input(7) == GPIO.HIGH and not button_pressed:
-        print("Someone pressed the alert button!")
-        button_pressed = True
-    elif GPIO.input(7) == GPIO.LOW:
-        button_pressed = False
-    time.sleep(0.1)
+{
+	"chat_id": "8679334855",
+    "text": "Someone Pressed alert Button"
+}
